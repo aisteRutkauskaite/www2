245790880b134ad5de4afaf4d753f668c21fbc8e
aisteRutkauskaite <aiste924@gmail.com>
2020-10-13 10:58:14 +0300

    belekas

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php date_default_timezone_set('Europe/Vilnius');?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Aš,  <?php print date('l') . 'ir PHP' ?></title>
+</head>
+<body>
+<h1><b>Aistė</b> - HTML <?php print  'ir PHP' ?> asas/ė jau nuo <?php print date('Y') . " metų" ?> </h1>
+<p>
+    Viskas prasidėjo <?php print date('m'). " mėnesio " .  date('d') . " dieną" ?>
+</p>
+</body>
+</html>
