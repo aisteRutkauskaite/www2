b0ff721b7fc8a9ef6f9c1a0e02468169b71a7ce0
aisteRutkauskaite <aiste924@gmail.com>
2020-10-15 15:26:52 +0300

    nesamone

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <?php date_default_timezone_set('Europe/Vilnius'); ?>
 <?php
-$time = date('h:i:s');
+//$time = date('h:i:s');
+//
+//$minute =  date('i:s') ;
+//
+//$reverse_minutes = abs(date(i) - 59);
+//$minutes_remaining = $reverse_minutes % 5;
+//$seconds_remaining = abs(date('s') - 59);
+//$timer_plus = date('s');
+//$timer_minus = 59 - date('s');
+//
+$joint_timer = 300 - (date('i') % 5 * 60 + date('s'));
 
-$second = date('s');
-$minute = date('i');
-$hour = date('h');
-
-$second_rotation = $second * 6 - 180;
-$minute_rotation = $minute * 6 - 180;
-$hour_rotation = $hour * 15 ;
-
-
+$joint_minutes = ($joint_timer - $joint_timer % 60) / 60;
+if ($joint_timer % 60 >= 10 ) {
+    $joint_seconds = $joint_timer % 60;
+} else {
+    $joint_seconds = '0' . $joint_timer % 60;
+}
+​
+$joint_percent = date('i') % 5 * 20 + 20 * date('s') / 60;
 ?>
 
 <!doctype html>
@@ -24,57 +33,35 @@ $hour_rotation = $hour * 15 ;
             box-sizing: border-box;
             margin: 0 auto;
         }
-
-        .clock {
-            background-image: url("https://i.dlpng.com/static/png/6810217_preview.png");
-            width: 500px;
-            height: 500px;
+        .joint{
+            background-image: url("joint/image.png");
+           width: 300px;
+            height: 300px;
             background-size: cover;
             position: relative;
+
+        }
+        .joint_color{
+            background-image: url("joint/image.color.jpg");
+            width: <?php print $joint_minutes  ; ?>px;
+            height: 300px;
+            background-size: cover;
+            position: absolute;
+            left: 42%;
+            top: 4%;
         }
 
-        .hours_arrow {
-            border: 5px solid black;
-            height: 200px;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform-origin: top left;
-            transform: rotate(<?php print $hour_rotation; ?>deg);
-        }
 
-        .minutes_arrow {
-            border: 3px solid green;
-            height: 150px;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform-origin: top left;
-            transform: rotate(<?php print $minute_rotation; ?>deg);
-        }
 
-        .seconds_arrow {
-            border: 1px solid red;
-            height: 100px;
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform-origin: top left;
-            transform: rotate(<?php print $second_rotation; ?>deg);
-        }
-        .clock_arrow{
-            right:0;
-            bottom: 0;
-        }
 
     </style>
 </head>
 <body>
-<h1><?php print $time; ?></h1>
-<div class="clock">
-    <div class="hours_arrow" ></div>
-    <div class="minutes_arrow " ></div>
-    <div class="seconds_arrow " ></div>
+
+<div class="">
+    <div class="joint" ></div>
+    <div class="joint_color" ></div>
+    <h2><?php print  $joint_seconds; ?></h2>
 </div>
 </body>
 </html>
