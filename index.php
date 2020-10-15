ce0311f28021a2a62e94eadcb8740c74fa9825cd
aisteRutkauskaite <aiste924@gmail.com>
2020-10-15 12:36:14 +0300

    clock

--- index.php
+++ index.php
@@ -1,25 +1,16 @@
+<?php date_default_timezone_set('Europe/Vilnius'); ?>
 <?php
-$rainy = rand(0, 1);
-$sunny = rand(0, 1);
-$class = '';
+$time = date('h:i:s');
 
-if ($rainy) {
-    if ($sunny) {
-        $text = 'rainy with sun';
-        $class = 'cloudy ';
-    } else {
-        $text = 'rainy';
-        $class = 'rainy';
-    }
-} else {
-    if ($sunny) {
-        $text = 'very sunny';
-        $class = 'very_sunny';
-    } else {
-        $text = 'sunny';
-        $class = 'sunny';
-    }
-}
+$second = date('s');
+$minute = date('i');
+$hour = date('h');
+
+$second_rotation = $second * 6 - 180;
+$minute_rotation = $minute * 6 - 180;
+$hour_rotation = $hour * 15 ;
+
+
 ?>
 
 <!doctype html>
@@ -30,36 +21,60 @@ if ($rainy) {
     <link href="https://fonts.googleapis.com/css2?family=Chilanka&display=swap" rel="stylesheet">
     <style>
         * {
-            font-family: 'Chilanka', cursive;
+            box-sizing: border-box;
+            margin: 0 auto;
         }
 
-        .sunny {
-            background-image: url("https://cdn3.iconfinder.com/data/icons/photography-54/64/sunny-mode-camera-photography-512.png");
+        .clock {
+            background-image: url("https://i.dlpng.com/static/png/6810217_preview.png");
+            width: 500px;
+            height: 500px;
+            background-size: cover;
+            position: relative;
         }
 
-        .cloudy {
-            background-image: url("https://png.pngtree.com/png-vector/20190214/ourlarge/pngtree-vector-cloudy-icon-png-image_450295.jpg");
-        }
-
-        .rainy {
-            background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSU6z5h3SW0QSZh90-IpIXbESstlr4XWr5cyg&usqp=CAU");
-        }
-
-        .very_sunny {
-            background-image: url("https://img.pngio.com/-very-hot-weather-png-512_512.png");
-        }
-
-        .img {
-            width: 300px;
+        .hours_arrow {
+            border: 5px solid black;
             height: 200px;
-            background-size: contain;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform-origin: top left;
+            transform: rotate(<?php print $hour_rotation; ?>deg);
         }
+
+        .minutes_arrow {
+            border: 3px solid green;
+            height: 150px;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform-origin: top left;
+            transform: rotate(<?php print $minute_rotation; ?>deg);
+        }
+
+        .seconds_arrow {
+            border: 1px solid red;
+            height: 100px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform-origin: top left;
+            transform: rotate(<?php print $second_rotation; ?>deg);
+        }
+        .clock_arrow{
+            right:0;
+            bottom: 0;
+        }
+
     </style>
 </head>
 <body>
-<div class="img <?php print $class; ?>">
-    <h2><?php print $text; ?></h2>
-
+<h1><?php print $time; ?></h1>
+<div class="clock">
+    <div class="hours_arrow" ></div>
+    <div class="minutes_arrow " ></div>
+    <div class="seconds_arrow " ></div>
 </div>
 </body>
 </html>
