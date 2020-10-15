e31675693faeafc5f093270efd998ee61ab489a3
aisteRutkauskaite <aiste924@gmail.com>
2020-10-15 10:34:06 +0300

    weather

--- index.php
+++ index.php
@@ -1,63 +1,25 @@
 <?php
-$date = date('Y-m-d', strtotime('+1 day'));
-$day = date('l', strtotime('+1 day'));
+$rainy = rand(0, 1);
+$sunny = rand(0, 1);
+$class = '';
 
-$breakfast1 = 3.4;
-$breakfast2 = 2.5;
-$breakfast3 = 3;
-
-$lunch1 = 5.7;
-$lunch2 = 7;
-$lunch3= 4;
-
-$dinner1 = 9;
-$dinner2= 8.7;
-$dinner3 = 5;
-
-
-$random1 = rand(1, 3);
-$random2 = rand(1, 3);
-$random3 = rand(1, 3);
-
-$total_sum = 0;
-
-if($random1 === 1){
-    $price1 = $breakfast1;
-    $total_sum += $breakfast1;
-} elseif ($random1 === 2) {
-    $price1 = $breakfast2;
-    $total_sum+= $breakfast2;
+if ($rainy) {
+    if ($sunny) {
+        $text = 'rainy with sun';
+        $class = 'cloudy ';
+    } else {
+        $text = 'rainy';
+        $class = 'rainy';
+    }
 } else {
-    $price1 = $breakfast3;
-    $total_sum += $breakfast3;
+    if ($sunny) {
+        $text = 'very sunny';
+        $class = 'very_sunny';
+    } else {
+        $text = 'sunny';
+        $class = 'sunny';
+    }
 }
-
-if($random2 === 1){
-    $price2 = $lunch1;
-    $total_sum += $lunch1;
-} elseif ($random2 === 2) {
-    $price2 = $lunch2;
-    $total_sum += $lunch2;
-} else {
-    $price2 = $lunch3;
-    $total_sum += $lunch3;
-}
-
-if($random3 === 1){
-    $price3 = $dinner1;
-    $total_sum += $dinner1;
-} elseif ($random3 === 2) {
-    $price3 = $dinner2;
-    $total_sum += $dinner2;
-} else {
-    $price3 = $dinner3;
-    $total_sum+= $dinner3;
-}
-
-
-//$total_price = $total1 + $total2 + $total3;
-$min_price_breakfast = min()
-
 ?>
 
 <!doctype html>
@@ -71,100 +33,36 @@ $min_price_breakfast = min()
             font-family: 'Chilanka', cursive;
         }
 
-        body {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
+        .sunny {
+            background-image: url("https://cdn3.iconfinder.com/data/icons/photography-54/64/sunny-mode-camera-photography-512.png");
         }
 
-        .breakfast1 {
-            background-image: url("food/breakfast1.jpg");
+        .cloudy {
+            background-image: url("https://png.pngtree.com/png-vector/20190214/ourlarge/pngtree-vector-cloudy-icon-png-image_450295.jpg");
         }
 
-        .breakfast2 {
-            background-image: url("food/breakfast2.png");
+        .rainy {
+            background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSU6z5h3SW0QSZh90-IpIXbESstlr4XWr5cyg&usqp=CAU");
         }
 
-        .breakfast3 {
-            background-image: url("food/breaksfast3.jpg");
+        .very_sunny {
+            background-image: url("https://img.pngio.com/-very-hot-weather-png-512_512.png");
         }
 
         .img {
             width: 300px;
-            height: 300px;
-            background-size: cover;
-        }
-
-        .lunch1 {
-            background-image: url("food/lunch1.jpg");
-        }
-
-        .lunch2 {
-            background-image: url("food/lunch2.png");
-        }
-
-        .lunch3 {
-            background-image: url("food/lunch3.jpg");
-        }
-
-        .dinner1 {
-            background-image: url("food/dinner1.jpg");
-        }
-
-        .dinner2 {
-            background-image: url("food/dinner2.jpg");
-        }
-
-        .dinner3 {
-            background-image: url("food/dinner3.jpg");
-        }
-
-        .container {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-        h3{
-            color: green;
+            height: 200px;
+            background-size: contain;
         }
     </style>
-
 </head>
 <body>
+<div class="img <?php print $class; ?>">
+    <h2><?php print $text; ?></h2>
 
-<h1> <?php print $date; ?></h1>
-<h3> <?php print $day; ?></h3>
-
-<section class="container">
-    <h2>Breakfast</h2>
-    <div class="img breakfast<?php print $random1; ?>"></div>
-    <h4>Breakfest price:<?php print  $price1; ?> </h4>
-    <?php  if($random1 === 2): ?>
-    <h3>Lowest price</h3>
-    <?php  endif ; ?>
-</section>
-
-<section class="container">
-    <h2>Lunch</h2>
-    <div class="img lunch<?php print $random2; ?>"></div>
-    <h4>Lunch price:<?php print $price2; ?> </h4>
-    <?php  if($random2 === 3): ?>
-        <h3>Lowest price</h3>
-    <?php  endif ; ?>
-</section>
-
-<section class="container">
-    <h2>Dinner</h2>
-    <div class="img dinner<?php print $random3; ?>"></div>
-    <h4>Dinner price:<?php print  $price3 ; ?> </h4>
-    <?php  if($random3 === 3): ?>
-        <h3>Lowest price</h3>
-    <?php  endif ; ?>
-</section>
-
-<h2>Total price:<?php print $total_sum; ?> </h2>
-
+</div>
 </body>
 </html>
 
+
+​
