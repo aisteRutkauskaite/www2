972ccc32d31aa279d82c52d14521d60f31138ff0
aisteRutkauskaite <aiste924@gmail.com>
2020-10-16 08:59:25 +0300

    sekundziu konvertavimas

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-$years = rand(1991, 2020);
+//$seconds = rand(0, 100000);
+// $string = gmdate('H:i:s' ,$seconds);
 
-if ($years % 400 === 0 || ($years % 4 === 0 && $years % 100 !== 0)) {
-    $answer = 'keliamieji';
-} else {
-    $answer = 'nekeliamieji';
-}
+$init = rand(0, 100000);
+$hours = floor($init / 3600);
+$minutes = floor(($init / 60) % 60);
+$seconds = $init % 60;
+
+$result= "$hours:$minutes:$seconds";
+
 ?>
 
 <!doctype html>
@@ -21,7 +24,10 @@ if ($years % 400 === 0 || ($years % 4 === 0 && $years % 100 !== 0)) {
 
 <div class="">
 
-    <h1><?php print $years ?> yra:<?php print $answer ?> </h1>
+<!--    <h1>--><?php //print $seconds ?><!-- </h1>-->
+<!--    <h1>--><?php //print $string ?><!-- </h1>-->
+    <h1><?php print $init ?> </h1>
+    <h1><?php print $result ?> </h1>
 </div>
 </body>
 </html>
