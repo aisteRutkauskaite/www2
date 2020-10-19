6ba882272dfdf43d290130e90c593f05cc6d1dc8
aisteRutkauskaite <aiste924@gmail.com>
2020-10-19 14:25:52 +0300

    kvadratai loop++

--- index.php
+++ index.php
@@ -9,22 +9,31 @@ $random_num = rand(6, 15);
     <title> variables </title>
     <link href="https://fonts.googleapis.com/css2?family=Chilanka&display=swap" rel="stylesheet">
     <style>
-        body{
+        body {
             display: flex;
-            flex-wrap: wrap;
+            flex-direction: column;
+            align-items: center;
         }
-        .container{
+
+        .container {
             width: 31vw;
             height: 250px;
             background-color: cadetblue;
             margin: 10px;
         }
+        section{
+            display: flex;
+            flex-wrap: wrap;
+        }
     </style>
 </head>
 <body>
-<?php for($x = 0; $x < $random_num; $x++ ) :?>
-<div class="container"></div>
-<?php endfor; ?>
+<h2>Iš viso kortelių <?php print $random_num; ?></h2>
+<section>
+    <?php for ($x = 0; $x < $random_num; $x++) : ?>
+        <div class="container"></div>
+    <?php endfor; ?>
+</section>
 </body>
 </html>
 
