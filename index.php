42ccd1ebb443b5bb0dc1efe1aa959c84b59b72a4
aisteRutkauskaite <aiste924@gmail.com>
2020-10-19 14:44:25 +0300

    zvaigxdutes loop++

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-$random_num = rand(6, 15);
+$random_num = rand(2, 100);
+
+for($w = 1; $w <= $random_num; $w++) {
+    for ($h = 1; $h <= $random_num; $h++) {
+        print '*' ;
+    }
+    print "<br /> " ;
+}
 ?>
 
 <!doctype html>
@@ -9,31 +16,9 @@ $random_num = rand(6, 15);
     <title> variables </title>
     <link href="https://fonts.googleapis.com/css2?family=Chilanka&display=swap" rel="stylesheet">
     <style>
-        body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .container {
-            width: 31vw;
-            height: 250px;
-            background-color: cadetblue;
-            margin: 10px;
-        }
-        section{
-            display: flex;
-            flex-wrap: wrap;
-        }
     </style>
 </head>
 <body>
-<h2>Iš viso kortelių <?php print $random_num; ?></h2>
-<section>
-    <?php for ($x = 0; $x < $random_num; $x++) : ?>
-        <div class="container"></div>
-    <?php endfor; ?>
-</section>
 </body>
 </html>
 
