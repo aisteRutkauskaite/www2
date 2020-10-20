48165a0c3ffa70f72173a2be85e6737f25cb7434
aisteRutkauskaite <aiste924@gmail.com>
2020-10-20 11:20:31 +0300

    cigareciu skaiciuokle++

--- index.php
+++ index.php
@@ -4,24 +4,28 @@ date_default_timezone_set('Europe/Vilnius');
 $days = 365;
 $pack_price = 3.50;
 $count_total = 0;
-$cigs_sat = rand(10, 20);
-$cigs_sun = rand(1, 3);
-$cigs_mon_fri = rand(3, 4);
 
-for($i = 1 ; $i <= $days ; $i++) {
+for($i = 0; $i <= $days ; $i++) {
     $weekday = (int) date("N", strtotime("+$i day"));
     if($weekday <= 5){
-        $count_total += $cigs_mon_fri ;
+        $cigs_mon_fri = rand(3, 4);
+        $Mon_friday_cigs = 0;
+        $count_total += $cigs_mon_fri;
+        $Mon_friday_cigs += $cigs_mon_fri;
     } elseif ($weekday == 6) {
+        $cigs_sat = rand(10, 20);
         $count_total +=  $cigs_sat;
     } else {
+        $cigs_sun = rand(1, 3);
         $count_total +=  $cigs_sun;
     }
 
 }
-$price_total = $pack_price * ceil( $count_total / 20);
-$h2 = "Per $days dienas, surūkysiu $count_total cigarečių už $price_total eur"
 
+$price_total = $pack_price * ceil( $count_total / 20);
+$saved_money = ceil($Mon_friday_cigs / 20) *$pack_price;
+$h2 = "Per $days dienas, surūkysiu $count_total cigarečių už $price_total eur";
+$h3 = "Nerukydamas darbo dienomis sutaupyciau $saved_money eur";
 ?>
 
 <!doctype html>
@@ -32,7 +36,8 @@ $h2 = "Per $days dienas, surūkysiu $count_total cigarečių už $price_total eu
 </head>
 <body>
 <h1> Mano dūmų skaičiuoklė</h1>
-<h2><?php print $h2 ;?> </h2>
+<h2><?php print $h2; ?> </h2>
+<h3><?php print $h3; ?> </h3>
 </body>
 </html>
 
