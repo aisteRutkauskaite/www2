7be127bde7f1f7692e2c1fd2ee237eb93d5bf1cb
aisteRutkauskaite <aiste924@gmail.com>
2020-10-21 12:26:41 +0300

    maistas

--- index.php
+++ index.php
@@ -1,37 +1,55 @@
 <?php
-$months = 24;
-$car_price_new = 30000;
-$depreciation = 2;
-$car_price_used = 30000;
+$food = [
+    "Shelve_one" => [
+        [
+            'name' => 'kiausiniai',
+            'vnt' => 3
+        ],
+        [
+            'name' => 'grietine',
+            'vnt' => 1
+        ],
+        [
+            'name' => 'jogurtas',
+            'vnt' => 2
+        ],
+        [
+            'name' => 'zuvis',
+            'vnt' => 0
+        ],
+    ],
+    "Shelve_two" => [
+        [
+            'name' => 'pienas',
+            'vnt' => 3
+        ],
+        [
+            'name' => 'kefyras',
+            'vnt' => 0
+        ],
+        [
+            'name' => 'degtine',
+            'vnt' => 2
+        ],
+        [
+            'name' => 'kecupas',
+            'vnt' => 2
+        ],
+    ]
+];
 
-for ($x = 0; $x < $months; $x++) {
-    $car_price_used -= $car_price_used * 2 / 100;
-    //$car_price_used *= (100 - $depreciation)/100;
-}
+var_dump($food);
 
-
-$car_price_used_format = number_format($car_price_used, 2);
-$depr_perc = number_format( ($car_price_new - $car_price_used) * 100 / $car_price_new, 2);
-
-$h2 = "Naujos mašinos kaina: $car_price_new eur";
-$h3 = "Po $months mėn, mašinos vertė bus: $car_price_used_format eur";
-$h4 = "Mašina nuvertės $depr_perc  proc.";
 ?>
+
 
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title> variables </title>
-    <style>
-
-    </style>
 </head>
 <body>
-<h1>Kiek nuvertės mašina ?</h1>
-<h2><?php print $h2 ?></h2>
-<h3><?php print $h3 ?></h3>
-<h4><?php print $h4 ?></h4>
 </body>
 </html>
 
