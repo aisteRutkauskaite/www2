72b46a491c55fe579df1f93387a6e0f38d93a5c8
aisteRutkauskaite <aiste924@gmail.com>
2020-10-21 14:37:56 +0300

    Eglute

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <?php
-$boulingas = [
-    [
-        'kegliai' => [
-            [true, false, false, true],
-            [false, false, true],
-            [false, true],
-            [true]
-        ],
-        'slapias' => false
-    ],
-    [
-        'kegliai' => [
-            [false, false, false, false],
-            [false, false, false],
-            [false, false],
-            [false]
-        ],
-        'slapias' => true
-    ],
-];
+$tree = rand(10, 30);
+$stem = rand(1, 4);
 
-var_dump($boulingas);
+for ($x = 1; $x <= $tree; $x++) {
+    for ($y = 1; $y <= $x; $y++) {
+        $decorations_rand = rand(1, 30);
+        if ($decorations_rand == 1) {
+            print '💚';
+        } elseif ($decorations_rand == 2) {
+            print '💗';
+        } elseif ($decorations_rand == 3) {
+            print '💙';
+        } else {
+            print '*/*';
+        }
+    }
+    print '</br>';
+}
+
+for ($i = 1; $i <= $stem; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        print '**';
+    }
+    print '</br>';
+}
 ?>
 
 
@@ -29,6 +32,14 @@ var_dump($boulingas);
 <head>
     <meta charset="UTF-8">
     <title> variables </title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
 </body>
