08a1a5d1afc56f9e25d78b0f6d4f9f844a4a64e0
aisteRutkauskaite <aiste924@gmail.com>
2020-10-27 15:44:58 +0200

    karusele

--- index.php
+++ index.php
@@ -1,42 +1,22 @@
 <?php
-$hours = 12;
-$ticket_price = 13;
-$money_earned = 0;
-$total_people = 0;
-$total_people_exit = 0;
-$carousel_spin = 0;
+$numbers = '20.3k 3.8k 7.7k 992';
 
-for ($x = 0; $x < $hours; $x++) {
-    $visitors = rand(1, 20);
-    if ($visitors < 10) {
-        $total_people_exit += $visitors;
-    } else {
-        $total_people_exit += $visitors - 10;
-        $carousel_spin++;
-        $total_people += 10;
-    }
+$numbers_array = explode(" ", $numbers);
+$numbers_array2 = [];
+
+foreach ($numbers_array as $index => $value) {
+    $numbers_array2[] = intval(str_replace([".", "k"], ["", "00"],  $value));
 }
-
-$money_earned += $ticket_price * $total_people;
-
-$h2 = "karusėlė uždirbo $money_earned eur";
-$h3 = "Žmonių prasisuko: $total_people";
-$h4 = "Žmonių išėjo: $total_people_exit";
-$h5 = "Karuselė sukosi $carousel_spin kartu";
+var_dump($numbers_array2);
 ?>
 
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title> variables </title>
+    <title> arrays </title>
 </head>
 <body>
-<h2><?php print $h2; ?></h2>
-<h3><?php print $h3; ?></h3>
-<h4><?php print $h4; ?></h4>
-<h5><?php print $h5; ?></h5>
-</body>
 </html>
 
 
