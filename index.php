12c196c04253c52ba35b23c7d8a3bb98b8823016
aisteRutkauskaite <aiste924@gmail.com>
2020-11-03 16:30:52 +0200

    forma.2020-11-03

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
-$sentence = 'vagi vagi vagi na';
-function change(&$sentence) {
-    $str_implode = str_split($sentence);
-    $caps = true;
-    foreach ($str_implode as $key => $letter) {
-        if ($caps) {
-            $out = strtoupper($letter);
-            if ($out !== " ") //not a space character
-                $caps = false;
-        } else {
-            $out = strtolower($letter);
-            $caps = true;
-        }
-        $str_implode[$key] = $out;
-    }
-    $sentence = implode('', $str_implode);
+$answer = '';
+
+if (isset($_POST['button'])) {
+    square($answer);
 }
-change($sentence);
-var_dump($sentence);
+
+function square(&$answer) {
+    if (isset($_POST['number'])) {
+        if (is_numeric($_POST['number'])) {
+            $answer = $_POST['number'] * $_POST['number'];
+        } else {
+            $answer = 'Please enter correct number';
+        }
+    }
+}
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -29,7 +26,11 @@ var_dump($sentence);
     <title>Functions</title>
 </head>
 <body>
-
+<form action="" method="post">
+    <input type="text" placeholder="number" name="number">
+    <button type="submit" name="button">Submit</button>
+</form>
+<h2><?php print $answer; ?></h2>
 </body>
 </html>
 
