7231b95876be3c4ac16299c9a11c60e5ce48c422
aisteRutkauskaite <aiste924@gmail.com>
2020-11-03 15:52:54 +0200

    letters

--- index.php
+++ index.php
@@ -1,27 +1,36 @@
 <?php
-$num = rand(-10, 10);
-$num2 = rand(-10, 10);
-
-function search_numb($number, $number2)
-{
-    var_dump($number, $number2);
-    if (($number > 0 && $number2 > 0) || ($number < 0 && $number2 < 0) || ($number === 0 && $number2 === 0)) {
-        return true;
+$sentence = 'vagi vagi vagi na';
+function change(&$sentence) {
+    $str_implode = str_split($sentence);
+    $caps = true;
+    foreach ($str_implode as $key => $letter) {
+        if ($caps) {
+            $out = strtoupper($letter);
+            if ($out !== " ") //not a space character
+                $caps = false;
+        } else {
+            $out = strtolower($letter);
+            $caps = true;
+        }
+        $str_implode[$key] = $out;
     }
-    return false;
-
+    $sentence = implode('', $str_implode);
 }
-
-var_dump(search_numb($num, $num2));
+change($sentence);
+var_dump($sentence);
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title> functions </title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Functions</title>
 </head>
 <body>
+
+</body>
 </html>
 
 
