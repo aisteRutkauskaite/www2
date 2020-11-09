8ac63788c280533f2ff698ae6e93257add3dd8ae
Aistė <71317858+aisteRutkauskaite@users.noreply.github.com>
2020-11-09 14:26:03 +0200

    forma+++

--- index.php
+++ index.php
@@ -1,44 +1,51 @@
 <?php
-$user_data = [];
+$users = [
+    [
+        'email' => 'aiste@gmail',
+        'password' => 'aiste'
+    ],
+    [
+        'email' => 'sigis@gmail',
+        'password' => 'sigis'
+    ],
+    [
+        'email' => 'agne@gmail',
+        'password' => 'agne'
+    ]
+];
 
-$thanks = "Dėkojame už jūsų pateiktą informaciją";
+$new_user = [
+    'email' => '',
+    'password' => ''
+];
 
-if (isset($_POST['button'])) {
-    if (!validate_field_not_empty($_POST['text'])) {
-        $error = 'Palikote tuščią žinutės laukelį';
-    } else {
-        $user_data['name'] = 'Vardas: ' . $_POST['name'];
-        $user_data['email'] = 'El. paštas: ' . $_POST['email'];
-        $user_data['$telephone'] = 'Telefono numeris: ' . $_POST['tel'];
-        if ($_POST['pardavimai'] == 'Pardavimai') {
-            $user_data['radio'] = 'Skirta: Pardavymų skyriui';
-        } elseif ($_POST['pardavimai'] == 'Administracija') {
-            $user_data['radio'] = 'Skirta: Administracijai';
-        } else {
-            $user_data['radio'] = 'Skirta: Klientų aptarnavimas';
-        }
-        if (isset($_POST['theme'])) {
-            if ($_POST['theme'] === 1) {
-                $user_data['$theme'] = ' Tema: Skundas';
-            } else {
-                $user_data['$theme'] = 'Tema: Pasiulymas';
-            }
-            $user_data['$message'] = 'Žinutė: ' . $_POST['text'];
-        }
-    }
-}
-
-function validate_field_not_empty($field_value)
+function validate_new_user(&$array, $email, $password)
 {
-    if ($field_value == '') {
-        return false;
+    foreach ($array as $user) {
+        if ($email === $user['email'] && $password === $user['password']) {
+            return false;
+        }
     }
+    $new_user = [
+        'email' => $email,
+        'password' => $password
+    ];
+    $array[] = $new_user;
+    var_dump($array);
     return true;
 }
 
+function passsword_match($password, $repaeated_password)
+{
+    if ($password === $repaeated_password) {
+        return true;
+    }
+    return false;
+}
+
+
+var_dump($users);
 ?>
-
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -46,72 +53,39 @@ function validate_field_not_empty($field_value)
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Form</title>
+    <title>Something</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Subrayada:wght@700&family=Nova+Square&display=swap"
+          rel="stylesheet">
 </head>
 <style>
-    body {
-        margin: 0 auto;
-        display: flex;
-        justify-content: center;
-    }
-
     form {
         display: flex;
         flex-direction: column;
-        width: 20%;
     }
 
-    input {
-        width: 100%;
-    }
-
-    label {
-        text-align: center;
-    }
-
-    .span {
-        font-weight: bold;
-        font-size: 25px;
-        text-align: center;
-    }
-
-    .container {
-        display: flex;
-        flex-direction: column;
-        text-align: center;
+    input, button {
+        width: 200px;
     }
 </style>
 <body>
-<form method="post">
-    <input type="text" name="name" value="" placeholder="Your name" required>
-    <input type="email" name="email" value="" placeholder="Your email" required>
-    <input type="tel" name="tel" value="" placeholder="Your telephone">
-
-    <span class="span"> Noriu susisiekti su: </span>
-    <input type="radio" id="pardavimai" name="pardavimai" value="Pardavimų skryrius" required>
-    <label for="pardavimai">Pardavimų skryrius</label>
-    <input type="radio" id="administracija" name="pardavimai" value="">
-    <label for="administracija">Administracija</label>
-    <input type="radio" id="klientai" name="pardavimai" value="">
-    <label for="klientai">Klientų aptarnavimo skyrius</label>
-
-    <select name="theme" id="theme">
-        <option disabled selected value>Select an option</option>
-        <option value="Pardavimai">Pardavimai</option>
-        <option value="Prekes">Prekes</option>
-    </select>
-
-    <textarea id="text" name="text" rows="4" cols="50" placeholder="Jūsų žinutė"></textarea>
-    <button name="button" type="submit">Siųsti</button>
-    <article class="container">
-        <h3>Dėkojame už jūsų žinutę, <?php print $_POST['name']; ?></h3>
-        <?php foreach ($user_data as $user_info): ?>
-            <p><?php print $user_info; ?></p>
-        <?php endforeach; ?>
-    </article>
+<form method="POST">
+    <label for="email">El. Paštas</label>
+    <input type="email" id="email" name="email" value="">
+    <label for="password">Slaptažodis</label>
+    <input type="password" id="password" name="password" value="">
+    <label for="password_repeat">Pakartokite slaptažodi</label>
+    <input type="password" id="password_repeat" name="password_repeat" value="">
+    <button type="submit" name="login" value="submit">Prisijungti</button>
+    ​
+    <?php if (!empty($_POST)): ?>
+        <?php if (passsword_match($_POST['password'], $_POST['password_repeat']) && validate_new_user($users, $_POST['email'], $_POST['password'])): ?>
+            <div class="message">Sveikiname prisiregistravus <?php print $_POST['email']; ?></div>
+        <?php else: ?>
+            <div class="error">Vartotojas - <i><?php print $_POST['email']; ?></i>
+                Toks vartotojas jau egzistuoja arba splatažodžiai nesutampa
+            </div>
+        <?php endif; ?>
+    <?php endif; ?>
 </form>
 </body>
 </html>
-
-
-​
